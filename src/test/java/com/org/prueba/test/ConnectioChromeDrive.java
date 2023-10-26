661ec0661b0e5cbfbdaad0a88ee16cd3a3fc2ca4
73943889 <149027019+73943889@users.noreply.github.com>
2023-10-25 21:19:04 -0500

    Connection
    Connection ChromeDirver

---
+++ src/test/java/com/org/prueba/test/ConnectioChromeDrive.java
@@ -0,0 +1,17 @@
+package com.org.prueba.test;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class ConnectioChromeDrive {
+
+	public static void main(String[] args) {
+		// validando ChromeDriver
+		
+		System.setProperty("webdriver.Chrome.driver","C:\\Users\\pc\\Documents\\chromedriver-win64\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.demoblaze.com/index.html");
+
+	}
+
+}
